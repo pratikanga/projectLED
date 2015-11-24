c752ec1be2cafdd7c0e76fc64213ddea17b4ced1
AJNEE <ajay4no1@gmail.com>
2015-11-24 12:59:46 +0530

    Added Code for Stepper motor LED

--- Experiment/wiringPi-d795066/BCM_TIMER.c
+++ Experiment/wiringPi-d795066/BCM_TIMER.c
@@ -2,49 +2,85 @@
 #include <stdio.h>
 #include <errno.h>
 
+int pin1 = 7;
 int pin2 = 5;
+int pin3 = ;
+int pin4 = ;
+
+int step = 0;
 
 void myInterrupt (void)
 {
-	printf("\n Interrupt Code started Executing \n");
-    delay(5000);
-    printf("Interrupt : LED On\n");
-    digitalWrite(pin2, 1);
-    delay(10000);
-    printf("Interrupt : LED Off\n");
-    digitalWrite(pin2, 0);
-  //  delay(1000);
-
+    printf("\n Interrupt Code started Executing \n");
+     
+    switch step
+    {
+        case 1 :
+            Start_Led_Cycle(pin2);
+	    break;
+	case 2 :
+	    Start_Led_Cycle(pin3);
+	    break;
+	case 3 :
+	    Start_Led_Cycle(pin4);
+	    break;
+	case 4 :
+	    step = 0;
+	    Start_Led_Cycle(pin1);
+            break;
+    }
 }
 
+void Start_Led_Cycle (int pin_number)
+{
+   step++;
+   printf("Turning LED %d ON\n",pin_number);
+   digitalWrite(pin_number, 1);
+   delay(10000);
+   printf("Turning LED %d Off\n",pin_number);
+   digitalWrite(pin_number, 0);
+}
 
 int main (void)
 {
     int pin1 = 7;
 	
-    printf("Raspberry Pi wiringPi blink test\n");
+    printf("Raspberry Pi My Stepper motor LED test\n");
 
     if (wiringPiSetup() == -1)
         return 1;
 
-//   pinMode(pin2, OUTPUT);
-// pinMode(pin1, OUTPUT);
 	
-    if (wiringPiISR (pin1, INT_EDGE_RISING, &myInterrupt) < 0)
+    if (wiringPiISR (pin1, INT_EDGE_FALLING, &myInterrupt) < 0)
     {
         fprintf (stderr, "Unable to setup ISR: %s\n", strerror (errno)) ;
         return 1 ;
     }
+
+    if (wiringPiISR (pin2, INT_EDGE_FALLING, &myInterrupt) < 0)
+    {  
+        fprintf (stderr, "Unable to setup ISR: %s\n", strerror (errno)) ;
+        return 1 ;
+    }
+
+    if (wiringPiISR (pin3, INT_EDGE_FALLING, &myInterrupt) < 0)
+    {
+        fprintf (stderr, "Unable to setup ISR: %s\n", strerror (errno)) ;
+        return 1 ;
+			    }
+
+    if (wiringPiISR (pin4, INT_EDGE_FALLING, &myInterrupt) < 0)
+    {
+        fprintf (stderr, "Unable to setup ISR: %s\n", strerror (errno)) ;
+        return 1 ;
+    }			    }
+     
     pinMode(pin1, OUTPUT);
     pinMode(pin2, OUTPUT);
-    for (;;)
-	{
-        printf("LED On\n");
-        digitalWrite(pin1, 1);
-        delay(10000);
-        printf("LED Off\n");
-        digitalWrite(pin1, 0);
-        delay(10000);
-    }
-  return 0;
+    pinMode(pin3, OUTPUT);
+    pinMode(pin4, OUTPUT);
+
+    Start_Led_Cycle(pin1);
+
+    return 0;
 }
