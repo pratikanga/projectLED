21ea05eb0694fdd699e7c11918e303f79db36d30
AJNEE <ajay4no1@gmail.com>
2015-11-28 22:54:11 +0530

    FIX 1

--- Experiment/wiringPi-d795066/BCM_TIMER.c
+++ Experiment/wiringPi-d795066/BCM_TIMER.c
@@ -8,8 +8,10 @@ int pin3 = 1; // GPIO-18 and pin-12
 int pin4 = 4; // GPIO-23 and pin-16
 
 static int step = 0;
-void Start_Led_Cycle(int );
+void Start_Led_Cycle(int , int );
+int timer = 10000;
 
+/*
 void myInterrupt (void)
 {
     step++;
@@ -18,19 +20,19 @@ void myInterrupt (void)
     switch (step)
     {
         case 1 :
-            print("\n In Case 1 : Step : %d Pin 7 is turning on \n",step);
+            print("\n In Case 1 : Step : %d Pin 18 is turning on \n",step);
             Start_Led_Cycle(pin2);
 	    break;
 	case 2 :
-            print("\n In Case 2 : Step : %d Pin 18 is turning on \n",step);
+            print("\n In Case 2 : Step : %d Pin 12 is turning on \n",step);
             Start_Led_Cycle(pin3);
 	    break;
 	case 3 :
-            print("\n In Case 3 : Step : %d Pin 12 is turning on \n",step);
+            print("\n In Case 3 : Step : %d Pin 16 is turning on \n",step);
             Start_Led_Cycle(pin4);
 	    break;
 	case 4 :
-            print("\n In Case 4 : Step : %d Pin 16 is turning on \n",step);
+            print("\n In Case 4 : Step : %d Pin 7 is turning on \n",step);
             step = 0;
 	    Start_Led_Cycle(pin1);
             break;
@@ -38,12 +40,41 @@ void myInterrupt (void)
 	    printf("\nSomething Wrong");
     }
 }
+*/
 
-void Start_Led_Cycle (int pin_number)
+void myInterrupt1( void )
 {
-   printf("Turning LED %d ON\n",pin_number);
+     step++;
+     print("\n In Case 1 : Step : %d Pin 18 is turning on \n",step);
+     Start_Led_Cycle(pin2);
+}
+
+void myInterrupt2(void)
+{
+     step++;
+     print("\n In Case 2 : Step : %d Pin 12 is turning on \n",step);
+     Start_Led_Cycle(pin3);
+}
+
+void myInterrupt3(void)
+{
+     step++;
+     print("\n In Case 3 : Step : %d Pin 16 is turning on \n",step);
+     Start_Led_Cycle(pin4);
+}
+
+void myInterrupt4(void)
+{
+     step = 0;
+     print("\n In Case 4 : Step : %d Pin 7 is turning on \n",step);
+     Start_Led_Cycle(pin1);
+}
+
+void Start_Led_Cycle (int pin_number, int delay)
+{
+   printf("Turning LED %d ON delay %d\n",pin_number,delay);
    digitalWrite(pin_number, 1);
-   delay(10000);
+   delay(delay);
    printf("Turning LED %d Off\n",pin_number);
    digitalWrite(pin_number, 0);
 }
@@ -56,25 +87,25 @@ int main (void)
         return 1;
 
 	
-    if (wiringPiISR (pin1, INT_EDGE_FALLING, &myInterrupt) < 0)
+    if (wiringPiISR (pin1, INT_EDGE_FALLING, &myInterrupt1) < 0)
     {
         fprintf (stderr, "Unable to setup ISR: %s\n", strerror (errno)) ;
         return 1 ;
     }
 
-    if (wiringPiISR (pin2, INT_EDGE_FALLING, &myInterrupt) < 0)
+    if (wiringPiISR (pin2, INT_EDGE_FALLING, &myInterrupt2) < 0)
     {  
         fprintf (stderr, "Unable to setup ISR: %s\n", strerror (errno)) ;
         return 1 ;
     }
 
-    if (wiringPiISR (pin3, INT_EDGE_FALLING, &myInterrupt) < 0)
+    if (wiringPiISR (pin3, INT_EDGE_FALLING, &myInterrupt3) < 0)
     {
         fprintf (stderr, "Unable to setup ISR: %s\n", strerror (errno)) ;
         return 1 ;
     }
 
-    if (wiringPiISR (pin4, INT_EDGE_FALLING, &myInterrupt) < 0)
+    if (wiringPiISR (pin4, INT_EDGE_FALLING, &myInterrupt4) < 0)
     {
         fprintf (stderr, "Unable to setup ISR: %s\n", strerror (errno)) ;
         return 1 ;
